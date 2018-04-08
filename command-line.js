233b9eff97fc43bc47a38a6ba783e916d9dd02ae
Diko <diko316@gmail.com>
2018-04-09 02:09:02 +0800

    finalized command-line js

--- command-line.js
+++ command-line.js
@@ -4,8 +4,36 @@
 
 var COMMANDO = require('./lib/index');
 var RUNNER = require('./lib/runner');
+var DOCS = require('./lib/docs');
+var command;
 
 
 require('./lib/jforge-init');
+require('./lib/jforge-config');
 
-RUNNER.run();
+
+command = RUNNER.extractCliOptions();
+if (command) {
+    command = command[0];
+
+    // go through initialize/configure process
+    if (command === 'init') {
+        RUNNER.runCommand('init');
+    }
+    // inspect configuration
+    else {
+        console.log('command ', command);
+
+        // run config
+        RUNNER.runCommand('config')
+
+            .then(function () {
+                return RUNNER.runCommand(command);
+            });
+    }
+}
+// show help
+else {
+    DOCS.show('usage');
+}
+
